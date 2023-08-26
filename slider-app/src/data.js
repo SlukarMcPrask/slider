41980c2bc0adb92cd5a9afb9bc0df5db6783de12
SlukarMcPrask <Nehulto91@gmail.com>
2023-08-26 15:25:03 +0200

    asd
    asd

---
+++ slider-app/src/data.js
@@ -0,0 +1,51 @@
+import image1 from "./images/movie1.png"
+import image2 from "./images/movie2.png"
+import image3 from "./images/movie3.png"
+import image4 from "./images/movie4.png"
+
+
+
+const allMovies = [
+    {
+        id: 1,
+        image: image1,
+        title: "Enola Holmesová 2",
+        age: "+13",
+        tags: "Vtipný * Napínavý * Rodina",
+        description: "Nulla pulvinar eleifend sem. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta",
+        category: "komedie",
+        link: "https://www.csfd.cz/film/1019026-enola-holmesova-2/prehled/"
+    },
+    {
+        id: 2,
+        image: image2,
+        title: "Enola Holmesová",
+        age: "+13",
+        tags: "Vtipný * Napínavý * Rodina",
+        description: "Etiam egestas wisi a erat. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Integer imperdiet lectus quis justo. Vivamus luctus egestas leo",
+        category: "komedie",
+        link: "https://www.csfd.cz/film/788685-enola-holmesova/prehled/"
+    },
+    {
+        id: 3,
+        image: image3,
+        title: "Rocknrolla",
+        age: "+16",
+        tags: "Gangster * Napínavý * Násilí",
+        description: "Nulla pulvinar eleifend sem. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nam libero tempore, cum soluta",
+        category: "romantický",
+        link: "https://www.csfd.cz/film/233547-rocknrolla/prehled/"
+    },
+    {
+        id: 4,
+        image: image4,
+        title: "Patriot",
+        age: "+16",
+        tags: "Gangster * Napínavý * Násilí",
+        description: "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Nulla quis diam. Nulla pulvinar eleifend sem. Maecenas lorem. Fusce suscipit libero eget.",
+        category: "romantický",
+        link: "https://www.csfd.cz/film/2575-patriot/prehled/"
+    },
+]
+
+export default allMovies
